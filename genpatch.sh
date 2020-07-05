0a4dda31bceb70fae3e0e95500c366ca4f5d3f02
NachiketUN <nachiketun8@gmail.com>
2020-07-05 12:24:55 +0530

    script for analysing non-merge commits in linux kernel v5.7

---
+++ genpatch.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+filename=tag.txt
+filelines=`cat $filename`  #reading the non-merge commit id file
+declare -A arr   #declaring a hashmap for storing different warning types
+messages_lines=`awk '{print $2}' message_types.txt` # reading the file which contains diff warning types
+for message in $messages_lines ;
+do
+  arr[$message]=0
+done
+
+#generates patch_file for every commit and runs checkpatch.pl
+#Aggregates errors and warnings using awk
+for line in $filelines ;
+do 
+ #echo $line
+ patch_file=$(git format-patch -1 --pretty=fuller $line 2>/dev/null)
+ patch_result=$(perl ./scripts/checkpatch.pl --show-types $patch_file)
+ rm -rf $patch_file  #deleting the generated patch file
+
+
+ errors=$(echo "$patch_result" | awk -F":" '/ERROR/{print $1}')
+ warnings=$(echo "$patch_result" | awk -F":" '/WARNING/{print $2}')
+
+ #Storing the warnings gathered in a hashmap
+ for warn in $warnings ;
+ do
+   if ! [[ -z $warn ]]; then
+   arr[$warn]=$((arr[$warn]+1))
+   fi
+ done
+ #if [[ "$line" == "bf71bc16e021" ]]; then
+ #  break
+ # fi
+done
+
+#Finding the most reported error
+max=Message
+min=Message
+for i in ${!arr[@]};
+do
+  echo $i ${arr[$i]} >> "error_report.txt"
+  (( ${arr[$i]} < ${arr[$min]} )) && min=$i
+  (( ${arr[$i]} > ${arr[$max]} )) && max=$i
+done
+echo "Most reported error is $max and it occurs ${arr[$max]} times"
+echo "Most reported error is $max and it occurs ${arr[$max]} times" >> "error_report.txt"
