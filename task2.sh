29e0f6f18df191c9228091eade7ebbf9354316c4
NachiketUN <nachiketun8@gmail.com>
2020-07-10 13:07:00 +0530

    Script to generate .csv file from checkpatch analysis

--- task2.sh
+++ task2.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-# Takes input of file containing commit IDs and outputs result.csv containing
-# columns of (commitID, SEVERITY, TYPE, FILENAME, LINE, EXACT ERROR) for every warning/error
-
 read -p 'Commits File: ' filename
 filelines=`cat $filename`  #reading the commit id file
 #generates patch_file for every commit and runs checkpatch.pl
@@ -18,10 +14,16 @@ do
 errors=$(echo "$patch_result" |awk -v li=$line -F":" '/ERROR/ {severity=$1; type=$2; desc=$3; getline; file=$3; line=$4; getline; print "\""li"\"" "| " "\""severity"\"" "| " "\""type"\"" "| " "\""file"\"" "| " "\""line"\"" "| " "\""desc"\"" "| " "\""$0"\"" "| "}')
 warnings=$(echo "$patch_result" |awk -v li=$line -F":" '/WARNING/ {severity=$1; type=$2; desc=$3; getline; file=$3; line=$4; getline; print "\""li"\"" "| " "\""severity"\"" "| " "\""type"\"" "| " "\""file"\"" "| " "\""line"\"" "| " "\""desc"\"" "| " "\""$0"\"" "| "}')
 if ! [[ -z "$errors" ]]; then
-echo $errors >> result.csv
+echo "$errors" >> result.csv
 fi
 
 if ! [[ -z "$warnings" ]]; then
 echo "$warnings" >> result.csv
+fi
+
+#if [[ "$line" == "b08418b54831" ]]; then
+#   break
+#fi
+
 done
 
