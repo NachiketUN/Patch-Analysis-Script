2828528a72af48cd4e9efcb06923d547d118bcd8
NachiketUN <nachiketun8@gmail.com>
2020-07-10 12:37:29 +0530

    Script to generate .csv file from checkpatch analysis

--- task2.sh
+++ task2.sh
@@ -11,23 +11,13 @@ do
  patch_result=$(perl ./scripts/checkpatch.pl --show-types $patch_file)
  rm -rf $patch_file  #deleting the generated patch file
 
-errors=$(echo "$patch_result" |awk -v li=$line -F":" '/ERROR/ {severity=$1;type=$2; desc=$3; 
-getline; file=$3; line=$4; getline; print "\x27"li"\x27" ", " 
-"\x27"severity"\x27" ", " "\x27"type"\x27" ", " "\x27"file"\x27" ", " "\x27"line"\x27" ", " "\x27"desc"\x27" ", " "\x27"$0"\x27"}')
-
-warnings=$(echo "$patch_result" |awk -v li=$line -F":" '/WARNING/ {severity=$1; type=$2; desc=$3; 
-getline; file=$3; line=$4; getline; print "\x27"li"\x27" ", " 
-"\x27"severity"\x27" ", " "\x27"type"\x27" ", " "\x27"file"\x27" ", " "\x27"line"\x27" ", " "\x27"desc"\x27"", " "\x27"$0"\x27"}')
+errors=$(echo "$patch_result" |awk -v li=$line -F":" '/ERROR/ {severity=$1; type=$2; desc=$3; getline; file=$3; line=$4; getline; print "\""li"\"" "| " "\""severity"\"" "| " "\""type"\"" "| " "\""file"\"" "| " "\""line"\"" "| " "\""desc"\"" "| " "\""$0"\"" "| "}')
+warnings=$(echo "$patch_result" |awk -v li=$line -F":" '/WARNING/ {severity=$1; type=$2; desc=$3; getline; file=$3; line=$4; getline; print "\""li"\"" "| " "\""severity"\"" "| " "\""type"\"" "| " "\""file"\"" "| " "\""line"\"" "| " "\""desc"\"" "| " "\""$0"\"" "| "}')
 if ! [[ -z "$errors" ]]; then
-echo "$errors" >> b.csv
+echo $errors >> result.csv
 fi
 
 if ! [[ -z "$warnings" ]]; then
-echo "$warnings" >> b.csv
-fi
- if [[ "$line" == "b08418b54831" ]]; then
-   break
-  fi
-
+echo "$warnings" >> result.csv
 done
 
